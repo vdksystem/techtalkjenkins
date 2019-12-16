d818717f374e0c460f246cd5b94c91e993033207
Kuleshov Dmitry <dkuleshov@lohika.com>
2019-12-16 20:46:08 +0200

    kubernetes template

--- srcipted.Jenkinsfile.groovy
+++ srcipted.Jenkinsfile.groovy
@@ -1,43 +1,46 @@
-node('jenkins-jenkins-slave') {
-    try {
-        def goHome
-        stage('Checkout') {
-            cleanWs()
-            checkout scm
-        }
-        stage('Test') {
-            goHome = tool name: 'go', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
-
-            withEnv(["GO_HOME=$goHome"]) {
-                sh '"$GO_HOME/go" get -u github.com/jstemmer/go-junit-report'
-                sh '"$GO_HOME/go" test -v 2>&1 | go-junit-report > report.xml'
+podTemplate(containers: [
+        containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat'),
+        containerTemplate(name: 'golang', image: 'golang:1.8.0', ttyEnabled: true, command: 'cat')
+]) {
+    node('jenkins-jenkins-slave') {
+        try {
+            def goHome
+            stage('Checkout') {
+                cleanWs()
+                checkout scm
             }
-        }
-        stage("Package") {
-            withEnv(["GO_HOME=$goHome"]) {
-                sh '"$GO_HOME/go" build'
+            stage('Test') {
+                container('golang') {
+                    sh '"$GO_HOME/go" get -u github.com/jstemmer/go-junit-report'
+                    sh '"$GO_HOME/go" test -v 2>&1 | go-junit-report > report.xml'
+                }
             }
-        }
-        stage("Publish") {
-            if (BRANCH_NAME == 'master') {
-                sh "aws ecr get-login --no-include-email"
-                sh "docker push"
+            stage("Package") {
+                container('golang') {
+                    sh '"$GO_HOME/go" build'
+                }
             }
-        }
-        stage("Deploy") {
-            if (BRANCH_NAME == 'master') {
-                sh "deploy to kubernetes"
+            stage("Publish") {
+                if (BRANCH_NAME == 'master') {
+                    sh "aws ecr get-login --no-include-email"
+                    sh "docker push"
+                }
             }
-        }
-    } catch (err) {
-        throw err
-    } finally {
-        switch (currentBuild.result) {
-            case 'SUCCESS':
-                echo "SUCCESS actions"
-                junit '*.xml'
-                archiveArtifacts 'app'
-                break
+            stage("Deploy") {
+                if (BRANCH_NAME == 'master') {
+                    sh "deploy to kubernetes"
+                }
+            }
+        } catch (err) {
+            throw err
+        } finally {
+            switch (currentBuild.result) {
+                case 'SUCCESS':
+                    echo "SUCCESS actions"
+                    junit '*.xml'
+                    archiveArtifacts 'app'
+                    break
+            }
         }
     }
 }
