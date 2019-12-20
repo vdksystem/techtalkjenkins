ee51f20e69eac33940d3b8cd8a72a58fec822e79
Kuleshov Dmitry <dkuleshov@lohika.com>
2019-12-20 12:41:22 +0200

    fix

--- srcipted.Jenkinsfile.groovy
+++ srcipted.Jenkinsfile.groovy
@@ -1,52 +1,37 @@
-podTemplate(yaml: """
-apiVersion: v1
-kind: Pod
-spec:
-  containers:
-  - name: golang
-    image: golang:1.8.0
-    command: ['cat']
-    tty: true
-""") {
-    node(POD_LABEL) {
-        try {
-            def goHome
-            stage('Checkout') {
-                cleanWs()
-                checkout scm
+node(POD_LABEL) {
+    try {
+        def goHome
+        stage('Checkout') {
+            cleanWs()
+            checkout scm
+        }
+        stage('Test') {
+            sh 'go get -u github.com/jstemmer/go-junit-report'
+            sh 'go test -v 2>&1 | go-junit-report > report.xml'
+        }
+        stage("Package") {
+            sh 'go build'
+        }
+        stage("Publish") {
+            if (BRANCH_NAME == 'master') {
+                sh "docker pull ubuntu:16.04"
+                sh "docker push dcr-docker-registry:5000/ubuntu"
             }
-            stage('Test') {
-                container('golang') {
-                    sh 'go get -u github.com/jstemmer/go-junit-report'
-                    sh 'go test -v 2>&1 | go-junit-report > report.xml'
-                }
+        }
+        stage("Deploy") {
+            if (BRANCH_NAME == 'master') {
+                echo "deploy to kubernetes"
             }
-            stage("Package") {
-                container('golang') {
-                    sh 'go build'
-                }
-            }
-            stage("Publish") {
-                if (BRANCH_NAME == 'master') {
-                    sh "docker pull ubuntu:16.04"
-                    sh "docker push dcr-docker-registry:5000/ubuntu"
-                }
-            }
-            stage("Deploy") {
-                if (BRANCH_NAME == 'master') {
-                    echo "deploy to kubernetes"
-                }
-            }
-        } catch (err) {
-            throw err
-        } finally {
-            switch (currentBuild.result) {
-                case 'SUCCESS':
-                    echo "SUCCESS actions"
-                    junit '*.xml'
-                    archiveArtifacts 'app'
-                    break
-            }
+        }
+    } catch (err) {
+        throw err
+    } finally {
+        switch (currentBuild.result) {
+            case 'SUCCESS':
+                echo "SUCCESS actions"
+                junit '*.xml'
+                archiveArtifacts 'app'
+                break
         }
     }
 }
