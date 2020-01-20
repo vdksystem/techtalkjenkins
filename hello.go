a67a0a493a94a5315c33078e78c030a98b235fe9
Kuleshov Dmitry <dkuleshov@lohika.com>
2020-01-21 01:41:12 +0200

    fixes

--- hello.go
+++ hello.go
@@ -6,5 +6,5 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcom to Lohika TechTalk!</h1>")
+	fmt.Fprint(w, "<h1>Welcom to Lohika TechTalks!</h1>")
 }
--- hello_test.go
+++ hello_test.go
@@ -29,7 +29,7 @@ func TestHealthCheckHandler(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := `<h1>Welcom to Lohika TechTalk!</h1>`
+	expected := `<h1>Welcom to Lohika TechTalks!</h1>`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
